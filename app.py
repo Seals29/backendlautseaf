db41baaca62c36a952ee1de4426fc05e3ae1f2f1
Seals29 <lionelriyadi11@gmail.com>
2025-01-14 22:01:26 +0700

    asdsa

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 import numpy as np
 import xgboost as xgb
 app = Flask(__name__)
-model = joblib.load('xgboost_model_water_salinity.pkl')
+
 @app.route('/')
 def index():
     return 'Hello World!'
@@ -24,7 +24,7 @@ def xgboost_model():
             return jsonify({'error': 'Missing one or more required features'}), 400
 
         features = np.array([data[key] for key in feature_keys]).reshape(1, -1)
-        
+        model = joblib.load('xgboost_model_water_salinity.pkl')
         prediction = model.predict(features)
         
         return jsonify({'prediction': prediction.tolist()})
@@ -42,7 +42,7 @@ def xgboost_model():
         data = request.get_json()
         print(data)
 
-        feature_keys = ['chlor_a_micrograms_per_l', 'water_temp', 'o2_conc_milimeters_per_liter', 'o2_sat', 
+        feature_keys = ['water_salinity', 'water_temp', 'o2_conc_milimeters_per_liter', 'o2_sat', 
                         'potential_density', 'phaeop_micrograms_per_l', 'phosphate_micromoles_per_l', 'silicate_micromoles_per_l', 
                         'nitrate_micromoles_per_l', 'nitrite_micromoles_per_l', 'year_part','month_part_cos', 'month_part_sin']
         
@@ -50,7 +50,7 @@ def xgboost_model():
             return jsonify({'error': 'Missing one or more required features'}), 400
 
         features = np.array([data[key] for key in feature_keys]).reshape(1, -1)
-        
+        model = joblib.load('./xgboost_model_chlor.pkl')
         prediction = model.predict(features)
         
         return jsonify({'prediction': prediction.tolist()})
@@ -69,38 +69,14 @@ def xgboost_model():
         print(data)
 
         feature_keys = ['chlor_a_micrograms_per_l', 'water_temp', 'o2_conc_milimeters_per_liter', 'o2_sat', 
-                        'potential_density', 'phaeop_micrograms_per_l', 'phosphate_micromoles_per_l', 'silicate_micromoles_per_l', 
+                        'potential_density', 'phaeop_micrograms_per_l', 'water_salinity', 'silicate_micromoles_per_l', 
                         'nitrate_micromoles_per_l', 'nitrite_micromoles_per_l', 'year_part','month_part_cos', 'month_part_sin']
         
         if not all(key in data for key in feature_keys):
             return jsonify({'error': 'Missing one or more required features'}), 400
 
         features = np.array([data[key] for key in feature_keys]).reshape(1, -1)
-        
-        prediction = model.predict(features)
-        
-        return jsonify({'prediction': prediction.tolist()})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-if __name__ == '__main__':
-    app.run(debug=True)
-
-@app.route('/xgboost/oxygenconc', methods=['POST'])
-def xgboost_model():
-    try:
-        # Get JSON data from the request
-        data = request.get_json()
-        print(data)
-
-        feature_keys = ['chlor_a_micrograms_per_l', 'water_temp', 'o2_conc_milimeters_per_liter', 'o2_sat', 
-                        'potential_density', 'phaeop_micrograms_per_l', 'phosphate_micromoles_per_l', 'silicate_micromoles_per_l', 
-                        'nitrate_micromoles_per_l', 'nitrite_micromoles_per_l', 'year_part','month_part_cos', 'month_part_sin']
-        
-        if not all(key in data for key in feature_keys):
-            return jsonify({'error': 'Missing one or more required features'}), 400
-
-        features = np.array([data[key] for key in feature_keys]).reshape(1, -1)
-        
+        model = joblib.load('./xgboost_model_phosphate.pkl')
         prediction = model.predict(features)
         
         return jsonify({'prediction': prediction.tolist()})
